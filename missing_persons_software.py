a05283cdf833cfb0d9329952f50d80af9455c028
Siddharth Rajasekharan <rajase.siddha02@svvsd.org>
2025-02-10 11:00:44 -0700

    Add files via upload

---
+++ missing_persons_software.py
@@ -0,0 +1,118 @@
+print("DISCLAIMER: THIS IS FOR EDUCATIONAL PURPOSES ONLY.")
+
+
+print("Hello! Welcome to the Missing Persons Software.")
+first_name = input("Can you enter the first name of the person you are looking for?")
+last_name = input("Can you enter the last name of the person you are looking for?")
+output_string_first = first_name[0]
+output_string_last = last_name[0]
+
+if output_string_first == 'A':
+    output_first_letter = '1'
+if output_string_first == 'B':
+    output_first_letter = '2'
+if output_string_first == 'C':
+    output_first_letter = '3'
+if output_string_first == 'D':
+    output_first_letter = '4'
+if output_string_first == 'E':
+    output_first_letter = '5'
+if output_string_first == 'F':
+    output_first_letter = '6'
+if output_string_first == 'G':
+    output_first_letter = '7'
+if output_string_first == 'H':
+    output_first_letter = '8'
+if output_string_first == 'I':
+    output_first_letter = '9'
+if output_string_first == 'J':
+    output_first_letter = '10'
+if output_string_first == 'K':
+    output_first_letter = '11'
+if output_string_first == 'L':
+    output_first_letter = '12'
+if output_string_first == 'M':
+    output_first_letter = '13'
+if output_string_first == 'N':
+    output_first_letter = '14'
+if output_string_first == 'O': 
+    output_first_letter = '15'
+if output_string_first == 'P':
+    output_first_letter = '16'
+if output_string_first == 'Q':
+    output_first_letter = '17'
+if output_string_first == 'R':
+    output_first_letter = '18'
+if output_string_first == 'S':
+    output_first_letter = '19'
+if output_string_first == 'T':
+    output_first_letter = '20'
+if output_string_first == 'U':
+    output_first_letter = '21'
+if output_string_first == 'V':
+    output_first_letter = '22'
+if output_string_first == 'W':
+    output_first_letter = '23'
+if output_string_first == 'X':
+    output_first_letter = '24'
+if output_string_first == 'Y':
+    output_first_letter = '25'
+if output_string_first == 'Z':
+    output_first_letter = '26'
+
+if output_string_last == 'A':
+    output_last_letter = '1'
+if output_string_last == 'B':
+    output_last_letter = '2'
+if output_string_last == 'C':
+    output_last_letter = '3'
+if output_string_last == 'D':
+    output_last_letter = '4'
+if output_string_last == 'E':
+    output_last_letter = '5'
+if output_string_last == 'F':
+    output_last_letter = '6'
+if output_string_last == 'G':
+    output_last_letter = '7'
+if output_string_last == 'H':
+    output_last_letter = '8'
+if output_string_last == 'I':
+    output_last_letter = '9'
+if output_string_last == 'J':
+    output_last_letter = '10'
+if output_string_last == 'K':
+    output_last_letter = '11'
+if output_string_last == 'L':
+    output_last_letter = '12'
+if output_string_last == 'M':
+    output_last_letter = '13'
+if output_string_last == 'N':
+    output_last_letter = '14'
+if output_string_last == 'O': 
+    output_last_letter = '15'
+if output_string_last == 'P':
+    output_last_letter = '16'
+if output_string_last == 'Q':
+    output_last_letter = '17'
+if output_string_last == 'R':
+    output_last_letter = '18'
+if output_string_last == 'S':
+    output_last_letter = '19'
+if output_string_last == 'T':
+    output_last_letter = '20'
+if output_string_last == 'U':
+    output_last_letter = '21'
+if output_string_last == 'V':
+    output_last_letter = '22'
+if output_string_last == 'W':
+    output_last_letter = '23'
+if output_string_last == 'X':
+    output_last_letter = '24'
+if output_string_last == 'Y':
+    output_last_letter = '25'
+if output_string_last == 'Z':
+    output_last_letter = '26'
+
+print("The coordinates of the missing person are(" + output_first_letter + "," + output_last_letter + ").")
+
+print("The coordinates of the missing person are(" + output_first_letter + "," + output_last_letter + ").")
