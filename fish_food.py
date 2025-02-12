58c42500e2d33a559b82bafcf9fa6f91c361be07
Siddharth Rajasekharan <rajase.siddha02@svvsd.org>
2025-02-12 10:28:55 -0700

    Add files via upload

---
+++ fish_food.py
@@ -0,0 +1,8 @@
+print("Hello! Welcome to the fish food calculating machine!")
+
+fish_food_amount = input("How much fish food do you have?")
+fish_food_amount = int(fish_food_amount)
+
+final_fish_food = fish_food_amount * fish_food_amount
+
+print("You can feed", final_fish_food, "fish with the amount of fish food you have!")
