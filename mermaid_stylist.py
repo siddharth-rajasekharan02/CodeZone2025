ec9b8e62bf38c669241959c1836dd6e03c641fff
Siddharth Rajasekharan <rajase.siddha02@svvsd.org>
2025-02-12 10:20:36 -0700

    Add files via upload

---
+++ mermaid_stylist.py
@@ -0,0 +1,12 @@
+print("Welcome to the Mermaid Stylist!")
+
+favorite_colour = input("What is your favorite colour?")
+
+if favorite_colour == 'red':
+    print("You should where a poppy in your hair!")
+elif favorite_colour == 'yellow':
+    print("You should where a sunflower in your hair!")
+elif favorite_colour == 'purple':
+    print("You should where a lavender in your hair!")
+else:
+    print("You're input is invalid. Please try again.")
