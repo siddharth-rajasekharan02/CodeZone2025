c32d4a29adbff970de4b78e2a1a006dd172f08a0
Siddharth Rajasekharan <rajase.siddha02@svvsd.org>
2025-02-18 10:20:49 -0700

    Add files via upload

---
+++ save_nemo.py
@@ -0,0 +1,22 @@
+print("Hello! Welcome to the Save Nemo Endangerment Status Finder!")
+
+
+animal = input("Enter the name of the animal you're looking for: ").lower()#+
+
+
+if animal == "blue whale":#+
+    print("The Blue Whale is endangered!")
+
+elif animal == "clownfish":#+
+    print("The Clownfish is of least concern.")
+
+elif animal == "angel shark":#+
+    print("The Angel Shark is critically endangered!")
+
+elif animal == "red stingray":#+
+    print("The Red Stingray is near threatened!")
+
+elif animal == "monk seal":#+
+    print("The Monk Seal is near extinct!")#+
+else:#+
+    print("Sorry, we don't have information about that animal.")#+
