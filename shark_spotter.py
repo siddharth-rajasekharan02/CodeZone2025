707968cf19db64765c7e446cd51086f694af9459
sircheatsalot24 <sach.pravir24@gmail.com>
2025-02-28 10:17:35 -0700

    Create shark_spotter.py

---
+++ shark_spotter.py
@@ -0,0 +1,8 @@
+size = input(""); size = int(size)
+if size < 30:
+    animal = "hammerhead whale"
+elif size > 30 and size < 40:
+    animal = "bass"
+elif size > 40 and size < 60:
+    animal = "shark"
+print(animal)
